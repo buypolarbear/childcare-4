e85c95d9109f6fc05de9b7423ef70733af098f78
Jacob Payne <heapalgorithm@gmail.com>
2014-01-29 19:27:10 -0500

    Added Ruby on Rails (RoR) app to repo under site/childcare

---
+++ site/childcare/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Childcare::Application.config.secret_key_base = '1b94ec030c07bb41e44ceffb3c5fcfc2c532d258e5c47006a0cf50ec7fccd28d5c63464742ccb84872d2a605a78e8cdca7de52fd91190c2c8052aa427634f393'
---
+++ site/childcare/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Childcare::Application.config.session_store :cookie_store, key: '_childcare_session'
