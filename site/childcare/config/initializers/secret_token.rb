e9c734f6b4e956b3a5c9e0d12a828c6677ebaabb
Jacob Payne <heapalgorithm@gmail.com>
2014-02-09 13:24:25 -0500

    Security token is now generated dynamically
    For security

--- site/childcare/config/initializers/secret_token.rb
+++ site/childcare/config/initializers/secret_token.rb
@@ -9,4 +9,22 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Childcare::Application.config.secret_key_base = '1b94ec030c07bb41e44ceffb3c5fcfc2c532d258e5c47006a0cf50ec7fccd28d5c63464742ccb84872d2a605a78e8cdca7de52fd91190c2c8052aa427634f393'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Childcare::Application.config.secret_key_base = secure_token
+
+# old (static) secret key)
+# Childcare::Application.config.secret_key_base = '1b94ec030c07bb41e44ceffb3c5fcfc2c532d258e5c47006a0cf50ec7fccd28d5c63464742ccb84872d2a605a78e8cdca7de52fd91190c2c8052aa427634f393'
