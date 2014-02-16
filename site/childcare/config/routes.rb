b48485fc730e342c7e078f9eb6df6babdb0713a6
Jacob Payne <heapalgorithm@gmail.com>
2014-02-16 16:03:48 -0500

    Generated site layout, static pages
    Generated static_pages controller and views, setup basic CSS files, and
    layouts, configured routes.

--- site/childcare/config/routes.rb
+++ site/childcare/config/routes.rb
@@ -1,5 +1,9 @@
 Childcare::Application.routes.draw do
 
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   root  'sessions#new'
