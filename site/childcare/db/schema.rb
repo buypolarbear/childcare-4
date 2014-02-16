10a260a539452987e8a4f14f4872ad3a172f262f
Jacob Payne <heapalgorithm@gmail.com>
2014-02-16 16:51:20 -0500

    Actually ran the migrations I generated in the last commit -.-

--- site/childcare/db/schema.rb
+++ site/childcare/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140216213529) do
+ActiveRecord::Schema.define(version: 20140216214220) do
 
   create_table "expenses", force: true do |t|
     t.string   "type"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20140216213529) do
     t.integer  "zip"
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "vehicles", force: true do |t|
     t.string   "make"
