9b73fcdccc1ac73806704d3bc173b546f1e44531
Jacob Payne <heapalgorithm@gmail.com>
2014-02-16 16:47:00 -0500

    Added fields and validations to users model

--- site/childcare/db/schema.rb
+++ site/childcare/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140131013452) do
+ActiveRecord::Schema.define(version: 20140216213529) do
 
   create_table "expenses", force: true do |t|
     t.string   "type"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20140131013452) do
     t.string   "city"
     t.string   "state"
     t.integer  "zip"
+    t.string   "password_digest"
   end
 
   create_table "vehicles", force: true do |t|
