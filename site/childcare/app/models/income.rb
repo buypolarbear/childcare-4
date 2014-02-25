795fd425a003caa03366b2d29d65ad5c5ee22810
Jacob Payne <heapalgorithm@gmail.com>
2014-02-24 22:10:48 -0500

    Added formatted getters to Income amount field

--- site/childcare/app/models/income.rb
+++ site/childcare/app/models/income.rb
@@ -2,6 +2,13 @@ class Income < ActiveRecord::Base
 	belongs_to :user
 	validates :user_id, presence: true
 	
+	def amount
+		ActionController::Base.helpers.number_to_currency(self[:amount])
+	end
+	
+	def amount_no_format
+		self[:amount]
+	end
 	
 	##
 	#def date  
