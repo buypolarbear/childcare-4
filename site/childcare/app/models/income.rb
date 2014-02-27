68247e628a61df465e2550270fd849a686e22727
Jacob Payne <heapalgorithm@gmail.com>
2014-02-26 23:01:43 -0500

    Added formatted date getters to the income model

--- site/childcare/app/models/income.rb
+++ site/childcare/app/models/income.rb
@@ -2,6 +2,8 @@ class Income < ActiveRecord::Base
 	belongs_to :user
 	validates :user_id, presence: true
 	
+	require 'date'
+	
 	
 	#############################################
 	# Getter for the income.amount field. Returns
@@ -16,17 +18,25 @@ class Income < ActiveRecord::Base
 	# Secondary getter for the income.amount field. 
 	# Returns a raw (decimal) version of the value.
 	#############################################
-	def amount_no_format
+	def amount_raw
 		self[:amount]
 	end
 	
-	##
+	
 	#def date  
-	# date.to_s(:db)  
-	#end  
-  
-	#def date=(date)  
-	#	self.date = Time.parse(date)
+	# self[:date].strftime("%m/%d/%Y")
 	#end
-	#
+	
+	def date( *format)
+        self[:date].strftime(format.first || "%m/%d/%Y")
+    end
+	
+	def date_raw
+	 self[:date]
+	end
+  
+	def date=(date)  
+		self[:date] = Date.parse(date)
+	end
+	
 end
