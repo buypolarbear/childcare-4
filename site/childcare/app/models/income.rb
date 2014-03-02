83ea03dada36794e5c202e40be565e2cb1029a31
Jacob Payne <heapalgorithm@gmail.com>
2014-03-02 14:23:52 -0500

    Manually edited merge conflicts

--- site/childcare/app/models/income.rb
+++ site/childcare/app/models/income.rb
@@ -2,8 +2,6 @@ class Income < ActiveRecord::Base
 	belongs_to :user
 	validates :user_id, presence: true
 	
-<<<<<<< HEAD
-=======
 	require 'date'
 	
 	
@@ -11,26 +9,10 @@ class Income < ActiveRecord::Base
 	# Getter for the income.amount field. Returns
 	# a currency-formatted version of the value.
 	##############################################
->>>>>>> origin/income_reporting
 	def amount
 		ActionController::Base.helpers.number_to_currency(self[:amount])
 	end
 	
-<<<<<<< HEAD
-	def amount_no_format
-		self[:amount]
-	end
-	
-	##
-	#def date  
-	# date.to_s(:db)  
-	#end  
-  
-	#def date=(date)  
-	#	self.date = Time.parse(date)
-	#end
-	#
-=======
 	
 	#############################################
 	# Secondary getter for the income.amount field. 
@@ -39,11 +21,6 @@ class Income < ActiveRecord::Base
 	def amount_raw
 		self[:amount]
 	end
-	
-	
-	#def date  
-	# self[:date].strftime("%m/%d/%Y")
-	#end
 	
 	def date( *format)
         self[:date].strftime(format.first || "%m/%d/%Y")
@@ -57,5 +34,4 @@ class Income < ActiveRecord::Base
 		self[:date] = Date.parse(date)
 	end
 	
->>>>>>> origin/income_reporting
 end
