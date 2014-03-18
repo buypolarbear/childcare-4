b58e086339e617424c3718145df3a8d3fe3fbf5f
summersb <bmsummers@ymail.com>
2014-03-18 12:36:50 -0400

    Added table sorting abilities to the pages and fixed a few links

--- site/childcare/app/assets/javascripts/application.js
+++ site/childcare/app/assets/javascripts/application.js
@@ -25,3 +25,15 @@
 $(document).on('change', '.submittable', function() {
   $(this).parents('form:first').submit();
 });
+
+$(document).ready(function() 
+    { 
+        $("#myTable").tablesorter(); 
+    } 
+); 
+$(document).ready(function() 
+    { 
+        $("#myTable").tablesorter( {sortList: [[0,0], [1,0]]} ); 
+    } 
+); 
+
