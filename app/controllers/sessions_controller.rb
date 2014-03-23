f0c16fb1de58d0d1b72714f0bf4a8492aa1f1f1a
Jacob Payne <heapalgorithm@gmail.com>
2014-03-23 18:27:23 -0400

    Can use either home phone or email to login
    - use same password no matter which method you use to login
    - the sign-in form only has one field (email or phone number in same
    one); the sessions controller automatically figures out if a phone
    number or an email was entered.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,18 @@ class SessionsController < ApplicationController
   # corresponds with the one passed in.
   ##############################################
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+	if is_email params[:session][:login_param]
+		user = User.find_by(email: params[:session][:login_param].downcase)
+	else
+		user = User.find_by(phone_home: params[:session][:login_param])
+	end
+	
     if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_back_or user
     else
-      flash.now[:error] = 'Invalid email/password combination'
-      render 'new'
+      flash[:error] = 'Invalid email or phone and password combination'
+      redirect_to root_url
     end
   end
 
@@ -25,5 +30,17 @@ class SessionsController < ApplicationController
   def destroy
 	sign_out
 	redirect_to root_url
+  end
+  
+  
+  def is_email(input)
+	valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	
+	# check to see if the input is a valid email address
+	if input =~ valid_email_regex
+		return true
+	else
+		return false # it must be a phone number
+	end
   end
 end
