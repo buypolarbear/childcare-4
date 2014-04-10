4a180fd261174d3c72315045ad3b914ba35061a3
Jacob Payne <heapalgorithm@gmail.com>
2014-04-10 00:43:17 -0400

    Cleaned up child signin views/controllers/assets

--- app/controllers/child_arrive_depart_controller.rb
+++ app/controllers/child_arrive_depart_controller.rb
@@ -1,9 +1,16 @@
 class ChildArriveDepartController < ApplicationController
+
   def last_names
-	@letters = ('A'..'Z').to_a
 	@children = Child.find(:all, :order => "lname desc")
   end
 
+  #############################################
+  # This is executed when the child_arrive_depart#children
+  # controller method is called with the "js"
+  # format (i.e. AJAX call instead of a redirect).
+  # Sets variables and passes rendering control
+  # on to children.js.erb
+  ##############################################
   def children
 	@child = Child.find(params[:id])
 	@parents = @child.parents.all
