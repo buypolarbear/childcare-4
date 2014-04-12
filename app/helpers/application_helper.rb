bdfabbf770dd9a35cfc2bcbd17aceb748ad37973
Jacob Payne <heapalgorithm@gmail.com>
2014-04-12 15:03:54 -0400

    Added helper method for generating nested forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+  
+  def link_to_add_fields(f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+  end
+  
 end
