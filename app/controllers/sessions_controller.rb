c720dfac9ba5c214b07708d6da5db45e8d5fc455
Jacob Payne <heapalgorithm@gmail.com>
2014-04-17 19:29:44 -0400

    Reverted to email signin
    - discovered a bug in the name-only signin related to users with the
    same name. Going back since all the users needing login (Lori and her
    employees) will have email addresses.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,46 +9,30 @@ class SessionsController < ApplicationController
   # corresponds with the one passed in.
   ##############################################
   def create
-	
-	name_array = params[:session][:login_param].split
-	
-	# get all the users with this full name
-	users = User.where("users.fname = ? AND users.lname = ?", name_array[0], name_array[1])
   
-    # use this code to log in with either an email address or a home phone number
-	##if !user && is_email params[:session][:login_param]
-	#	user = User.find_by(email: params[:session][:login_param].downcase)
-	#else
-	#	user = User.find_by(phone_home: params[:session][:login_param])
-	#end
-	#
-	
-	# if there is at least 1 user in the database with the passed name
-    if users.count > 0
-	
-		# loop through all users with the passed name and try logging them in with the passed password
-		users.each do |user|
-			if user.authenticate(params[:session][:password])
-				sign_in user
-				redirect_back_or user
-			end
-		end		
-		
+	user = User.find_by(email: params[:session][:login_param].downcase)
+
+	if user && user.authenticate(params[:session][:password])
+		sign_in user
+		redirect_back_or user		
     else
-		flash[:error] = 'There is no user with the name #{params[:session][:login_param]}'
+		flash[:error] = "Invalid email address or password"
 		redirect_to root_url
     end
   end
 
   #############################################
-  # 
+  # Signs out the current user
   ##############################################
   def destroy
 	sign_out
 	redirect_to root_url
   end
   
-  
+  #############################################
+  # Checks the passed string to see if it is a
+  # well-formed email address
+  ##############################################
   def is_email(input)
 	valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
